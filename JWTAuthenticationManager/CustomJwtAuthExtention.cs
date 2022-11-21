7feaeaabc7ef1c6bed145108838c12e780e4dc77
Navjyot Gurhale <navjyot.gurhale0@gmail.com>
2022-11-21 16:16:39 +0530

    JwtAuthManager

---
+++ JWTAuthenticationManager/CustomJwtAuthExtention.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthenticationManager
+{
+    public static class CustomJwtAuthExtention
+    {
+        public static void AddCustomJwtAuthentication(this IServiceCollection services)
+        {
+            services.AddAuthentication(x => {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JWTTokenHander.JWT_Security_Key)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+        }
+    }
+}
---
+++ JWTAuthenticationManager/JWTTokenHander.cs
@@ -0,0 +1,67 @@
+﻿using JWTAuthenticationManager.Models;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JWTAuthenticationManager
+{
+    public class JWTTokenHander
+    {
+        public const string JWT_Security_Key = "DweFEfqEfweEFefwefwef33423sdfEDFWEf";
+        private const int JWT_Token_Validity_Mins = 20;
+        private readonly List<UserAccount> _userAccountsList;
+
+        public JWTTokenHander()
+        {
+            _userAccountsList = new List<UserAccount>
+            {
+                new UserAccount { UserName = "admin", Password = "Admin@123", Role = "Admin" },
+                new UserAccount { UserName = "user01", Password = "User01@123", Role = "User" }
+            };
+        }
+
+        public AuthenticationResponse? GenerateJWTToken(AuthenticationRequest authenticationRequest)
+        {
+            if (string.IsNullOrWhiteSpace(authenticationRequest.UserName) || string.IsNullOrWhiteSpace(authenticationRequest.Password))
+                return null;
+
+            /*Validation*/
+            var userAccount = _userAccountsList.Where(x => x.UserName == authenticationRequest.UserName && x.Password == authenticationRequest.Password).FirstOrDefault();
+            if (userAccount == null) return null;
+
+            var tokenExpiryTimeStamp = DateTime.Now.AddMinutes(JWT_Token_Validity_Mins);
+            var tokenKey = Encoding.ASCII.GetBytes(JWT_Security_Key);
+            var claimsIdentity = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Name, authenticationRequest.UserName),
+                new Claim(ClaimTypes.Role, userAccount.Role)
+            });
+            var signingCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(tokenKey),
+                SecurityAlgorithms.HmacSha256Signature
+            );
+            var securityTokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claimsIdentity,
+                Expires=tokenExpiryTimeStamp,
+                SigningCredentials = signingCredentials
+            };
+
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = jwtSecurityTokenHandler.CreateToken(securityTokenDescriptor);
+            var token = jwtSecurityTokenHandler.WriteToken(securityToken);
+
+            return new AuthenticationResponse
+            {
+                UserName = userAccount.UserName,
+                ExpiresIn = (int)tokenExpiryTimeStamp.Subtract(DateTime.Now).TotalSeconds,
+                JWTToken = token
+            };
+        }
+    }
+}
