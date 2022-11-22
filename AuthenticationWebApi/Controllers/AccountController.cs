0e2fcaf9d90f3787c9e0113b0baaf11619e00818
Navjyot Gurhale <navjyot.gurhale0@gmail.com>
2022-11-22 15:30:06 +0530

    Ocelot Microservice with 1 JWT auth

---
+++ AuthenticationWebApi/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using JWTAuthenticationManager;
+using JWTAuthenticationManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        private readonly JWTTokenHander _jWTTokenHander;
+
+        public AccountController(JWTTokenHander jWTTokenHander)
+        {
+            this._jWTTokenHander = jWTTokenHander;
+        }
+
+        [HttpPost]
+        public ActionResult<AuthenticationResponse> Authenticate(AuthenticationRequest authenticationRequest)
+        {
+            var authenticationResponse = _jWTTokenHander.GenerateJWTToken(authenticationRequest);
+            if (authenticationResponse == null) return Unauthorized();
+
+            return authenticationResponse;
+        }
+    }
+}
